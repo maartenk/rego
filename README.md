8901c11dfe3408e86f58d9cff1724b794671950d
Davide Vaghetti <davide.vaghetti@garr.it>
2018-10-23 13:48:22 +0200

    missing files - see previous commit

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
 # rego
+
+rego is a registry application for OpenID Connect federations and entities.
+
+## Install & run
+
+Clone the github repo
+
+```
+git clone https://github.com/daserzw/rego
+```
+
+Create a virtualenv and activate it
+
+```
+python3 -mvenv venv
+. venv/bin/activate
+```
+
+Install the app with pip (it will nicely handle dependencies)
+
+```
+pip install -e .
+```
+
+Run it
+
+```
+export FLASK_APP=rego
+export FLASK_ENV=development
+flask run
+```
+
+Open a browser and navigate to `http://127.0.0.1:5000`.
--- rego/models.py
+++ rego/models.py
@@ -1,12 +1,10 @@
-from flask_sqlalchemy import SQLAlchemy
-
-db = SQLAlchemy()
+from rego import db
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
+    name = db.Column(db.String(255))
     password_hash = db.Column(db.String)
-    email = db.Column(db.String)
+    email = db.Column(db.String(254))
 
 class Role(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -20,12 +18,25 @@ class Organization(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
 
-
 class Contact(db.Model):
-    pass
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(254))
+    contact_type_id = db.Column(
+        db.Integer, db.ForeignKey('contact_type.it'), nullable=False
+    )
+                              
+    contact_type = db.relationship(
+        'ContactType',
+        backref=db.backref('contacts', lazy=True)
+    )
 
 class ContactType(db.Model):
-    pass
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), unique=True)
 
 class Entity(db.Model):
-    pass
+    id = db.Column(db.Integer, primary_key=True)
+    entity_id = db.Column(db.String(1024), unique=True)
+    data = db.Column(db.Text)
+
+    
